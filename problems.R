d20878fc34a8f09f2954fc5dabb4608a29abbd32
jjureta <Josip.jureta@gmail.com>
2017-01-10 22:10:12 -0500

    add unit tests

--- problems.R
+++ problems.R
@@ -1,3 +1,5 @@
+library(testthat)
+
 source("./learning.R")
 
 # P 4.1
@@ -8,7 +10,11 @@ p <- data.frame(p0 = c(0, 1, 1, 1, 0, -1,-1,-1),
 t <- data.frame(t = c(1, 0, 0, 0, 0, 1, 1, 1))
 
 w <- perceptron(as.matrix(p),  as.matrix(t), hard_limit_transfer)
-print(w)
+expected <- list(w = matrix( c(-2, 1, -1), nrow = 3, ncol = 1), updates = 5)
+
+test_that("Test perceptron P 4.1 a",{
+  expect_equal( w$w, expected$w)
+})
 
 # b.
 p <- data.frame(p0 = c(0, 2, 2, 2, 0, -2,-2,-2),
@@ -17,7 +23,11 @@ p <- data.frame(p0 = c(0, 2, 2, 2, 0, -2,-2,-2),
 t <- data.frame(t = c(0, 0, 0, 1, 1, 1, 0, 0))
 
 w <- perceptron(as.matrix(p),  as.matrix(t), hard_limit_transfer)
-print(w)
+expected <- list(w = matrix( c(0, -2, -1), nrow = 3, ncol = 1), updates = 1)
+
+test_that("Test perceptron P 4.1 b",{
+  expect_equal( w$w, expected$w)
+})
 
 # c.
 p <- data.frame(p0 = c(0, 1, 1, 1, 0, -1,-1,-1),
@@ -26,4 +36,10 @@ p <- data.frame(p0 = c(0, 1, 1, 1, 0, -1,-1,-1),
 t <- data.frame(t = c(1, 1, 1, 1, 1, 1, 1, 0))
 
 w <- perceptron(as.matrix(p),  as.matrix(t), hard_limit_transfer)
-print(w)
+expected <- list(w = matrix( c(1, -1, 1), nrow = 3, ncol = 1), updates = 5)
+
+test_that("Test perceptron P 4.1 c",{
+  expect_equal( w$w, expected$w)
+})
+
+# P 4.3
