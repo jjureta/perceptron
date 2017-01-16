cbba6eb27995d1bb81e5005abac199c7ab326099
jjureta <josip.jureta@gmail.com>
2017-01-15 19:37:14 -0500

    use matrices

--- problems.R
+++ problems.R
@@ -7,23 +7,18 @@ source("./learning.R")
 p <- matrix(c(0,1,1,1,1,0,1,-1,0,-1,-1,-1,-1,0,-1,1), 2, 8)
 t <- matrix(c(1, 0, 0, 0, 0, 1, 1, 1), 8, 1)
 
-w <- perceptron(as.matrix(p),  as.matrix(t), hard_limit_transfer)
+w <- perceptron( p,  t, hard_limit_transfer)
 expected <- list(w = matrix( c(-2, 1, -1), nrow = 3, ncol = 1), updates = 5)
-
-print(w)
-print(expected)
 
 test_that("Test perceptron P 4.1 a",{
   expect_equal( w$w, expected$w)
 })
 
 # b.
-p <- data.frame(p0 = c(0, 2, 2, 2, 0, -2,-2,-2),
-                p1 = c(2, 2, 0,-2,-2, -2, 0, 2))
+p <- matrix(c(0,2,2,2,2,0,2,-2,0,-2,-2,-2,-2,0,-2,2), 2, 8)
+t <- matrix(c(0, 0, 0, 1, 1, 1, 0, 0), 8, 1)
 
-t <- data.frame(t = c(0, 0, 0, 1, 1, 1, 0, 0))
-
-w <- perceptron(as.matrix(p),  as.matrix(t), hard_limit_transfer)
+w <- perceptron( p,  t, hard_limit_transfer)
 expected <- list(w = matrix( c(0, -2, -1), nrow = 3, ncol = 1), updates = 1)
 
 test_that("Test perceptron P 4.1 b",{
@@ -31,13 +26,11 @@ test_that("Test perceptron P 4.1 b",{
 })
 
 # c.
-p <- data.frame(p0 = c(0, 1, 1, 1, 0, -1,-1,-1),
-                p1 = c(1, 1, 0,-1,-1, -1, 0, 1))
+p <- matrix(c(0,1,1,1,1,0,1,-1,0,-1,+1,-1,-1,0,-1,1), 2, 8)
+t <- matrix(c(1, 1, 1, 1, 1, 1, 1, 0), 8, 1)
 
-t <- data.frame(t = c(1, 1, 1, 1, 1, 1, 1, 0))
-
-w <- perceptron(as.matrix(p),  as.matrix(t), hard_limit_transfer)
-expected <- list(w = matrix( c(1, -1, 1), nrow = 3, ncol = 1), updates = 5)
+w <- perceptron( p, t, hard_limit_transfer)
+expected <- list(w = matrix( c(2, -1, 2), nrow = 3, ncol = 1), updates = 5)
 
 test_that("Test perceptron P 4.1 c",{
   expect_equal( w$w, expected$w)
